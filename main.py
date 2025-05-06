8986eca69c783519dd0d93e9b197497131baf4e3
houlton888 <houlton888@gmail.com>
2025-05-06 14:55:49 -0400

    updates
    eli updates

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import time
 wifi = {'ssid':"Tufts_Robot",'pass':''}
 # wifi = Fairmount_Wireless = {'ssid':'Verizon_SLD76X','pass':'dice7-gee-prate'}
 # #wifi = {'ssid':'Natalie07','pass':'Blue126Kitchen'}
-# #IP_add = '10.243.115.106' #Tufts_Secure
-IP_add = '10.247.137.157' #Tufts Robot
+IP_add = '10.243.114.200' #Tufts_Secure
+#IP_add = '10.247.137.157' #Tufts Robot
 # IP_add = "192.168.1.235" #home
 
 bot = TeamWDreamBot(wifi, IP_add)
